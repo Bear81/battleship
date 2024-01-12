75a6bd5fa2873706036c834c6c0bb330a6ce11e6
Bear81 <>
2024-01-12 16:16:19 +0000

    Add functions to build empty game grid

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import randon
+
+
+def build_grid(size):
+    """
+    Create a size x size grid, initialised with a water symbol
+    """
+    grid = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append("~")
+        grid.append(row)
+    return grid
+
+
+def print_grid():
+    """
+    Print the grid
+    """
+    for row in grid:
+        print(" ".join(row))
+    print()
+
+
+# Create abd print an empty 10x10 grid
+grid_size = 10
+grid = build_grid(grid_size)
+print_grid(grid)
